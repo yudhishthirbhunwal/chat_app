60635c1570e6d53f4552d77ddf4e25aa7b933c1e
Yudhishthir <yudhishthir@poplify.com>
2023-06-07 19:04:29 +0530

    [Devise User]
    - Added devise User model with username attribute.

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  # Validations
+  validates :username, presence: true, uniqueness: true
+end
